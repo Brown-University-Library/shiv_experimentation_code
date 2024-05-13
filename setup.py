c204297c50d57176e1a9d5f7d598e6f8122b22ae
Birkin James Diana <birkin.diana@gmail.com>
2024-05-12 20:17:11 -0400

    calls proper main-module.

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     description="Uses public repository API to count all hall-hoag org-items.",
     py_modules=["count_orgs"],
     entry_points={
-        "console_scripts": ["count_orgs=count_orgs:main"],
+        "console_scripts": ["count_orgs=count_orgs:prep_org_count"],
     },
     install_requires=['requests==2.26.0'],
 )
